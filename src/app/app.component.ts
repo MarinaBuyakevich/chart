f369cab5dfc50b27609bd0e05a0672760bd03080
MarinaBuyakevich <manyunya1011@gmail.com>
2019-01-14 11:31:47 +0200

    chart form is made

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
+
 export class AppComponent {
-  title = 'chart';
+  Appearance = true;
+  Bar = true;
+  Chart = true;
+  Legend = true;
+  button_1 = false;
+  button_2 = false;
+  button_3 = false;
+  button_4 = false;
 }
+
